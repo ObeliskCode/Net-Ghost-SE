b15326d42f5a38412bab9eb7f4b14df3854335b5
Noel Garcia <n6garcia@ucsd.edu>
2024-04-24 22:21:48 -0700

    particle instancing

---
+++ Source/ParticleSystem.h
@@ -0,0 +1,29 @@
+#ifndef PARTICLESYSTEM_H
+#define PARTICLESYSTEM_H
+
+#include <vector>
+#include "glm/glm.hpp"
+#include "Texture.h"
+#include "Particle.h"
+
+// class for instancing particles 
+
+class ParticleSystem {
+	public:
+		ParticleSystem();
+		~ParticleSystem();
+		void DrawParticles(Shader& shader, Camera& camera);
+		void updateParticles(float delta);
+
+	private:
+		std::vector <float> Vert;
+		std::vector <GLuint> Ind;
+		GLuint VAO, VBO, EBO;
+
+		Texture* sprite;
+
+		std::vector <Particle> particles;
+};
+
+
+#endif
