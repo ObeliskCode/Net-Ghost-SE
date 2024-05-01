9e3f36c510ccad68ac4ea74e061ba3676953a5bf
Noel Garcia <n6garcia@ucsd.edu>
2024-04-30 22:27:04 -0700

    comments

--- Source/Mesh.cpp
+++ Source/Mesh.cpp
@@ -64,6 +64,7 @@ void Mesh::Draw(
 		textures[i].Bind();
 	}
 
+	/* SEND THESE TO ALL SHADERS ONCE BEFORE DRAWING */
 	glUniformMatrix4fv(glGetUniformLocation(shader.ID, "lightSpaceMatrix"), 1, GL_FALSE, glm::value_ptr(Globals::get().lightSpaceMatrix));
 
 	glActiveTexture(GL_TEXTURE0 + 6);
@@ -79,6 +80,7 @@ void Mesh::Draw(
 	// Take care of the camera Matrix  // ONLY needs to be sent once for shader!! (instancing)
 	glUniform3f(glGetUniformLocation(shader.ID, "camPos"), camera.getPosition().x, camera.getPosition().y, camera.getPosition().z);
 	camera.Matrix(shader, "camMatrix");
+	/* SEND THESE TO ALL SHADERS ONCE BEFORE DRAWING */
 
 	// Push the matrices to the vertex shader
 	glUniformMatrix4fv(glGetUniformLocation(shader.ID, "transform"), 1, GL_FALSE, glm::value_ptr(transform));
