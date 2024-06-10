eb9b889c8938af2f889e34829c06cb8605e3c55a
Noel Garcia <n6garcia@ucsd.edu>
2024-06-09 17:10:50 -0700

    comment

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -2,7 +2,7 @@
 
 // two functions queueScene and endScene?
 
-// FINAL GRAHPICS TODO: screen-space shadows & frustrum culling & updated rendering pipeline (deferred/forward+)
+// FINAL GRAHPICS TODO: screen-space shadows & frustrum culling & updated rendering pipeline (deferred/forward+) & MT rendering
 
 int main(int argc, char **argv) {
     if (argc > 1){
