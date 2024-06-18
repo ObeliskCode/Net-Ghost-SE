93215b22ac1e19fb23ff4e74a4b103f020831811
Noel Garcia <n6garcia@ucsd.edu>
2024-06-17 21:51:55 -0700

    Daemon.h added

---
+++ Source/Daemon.h
@@ -0,0 +1,26 @@
+#ifndef DAEMON_H
+#define DAEMON_H
+
+// gross class (to be refactored), breaks if you include it in files you include above!
+class Daemon {
+public:
+    // defines an class operation that lets clients access its unique instance.
+    static Daemon& get() {
+        // may be responsible for creating its own unique instance.
+        if (nullptr == instance) instance = new Daemon();
+        return *instance;
+    }
+    Daemon(const Daemon&) = delete; // rule of three
+    Daemon& operator=(const Daemon&) = delete;
+    static void destruct() {
+        delete instance;
+        instance = nullptr;
+    }
+
+private:
+    Daemon() { } // no public constructor
+    ~Daemon() = default; // no public destructor
+    static Daemon* instance; // declaration class variable
+
+};
+#endif
