8257650d50e5d6f322db5b9937ff7add0fc5fbab
brent hartshorn <brenthartshorn@proton.me>
2024-09-02 16:29:46 -0700

    Model helper function from_memory

--- Source/Model.h
+++ Source/Model.h
@@ -12,28 +12,29 @@
 class Model
 {
 public:
-    Model(const char* path)
-    {
-        loadModel(path);
-    }
-    ~Model();
-    void Draw(Shader& shader, Camera& camera,
-        glm::mat4& transform, glm::mat4& ntransform);
+	Model(const char* path)
+	{
+		loadModel(path);
+	}
+	~Model();
+	void from_memory(std::string);
+	void Draw(Shader& shader, Camera& camera,
+		glm::mat4& transform, glm::mat4& ntransform);
 
-    void DrawShadow(Shader& shader, glm::mat4& transform);
+	void DrawShadow(Shader& shader, glm::mat4& transform);
 
-    Material mat; // this mat here may have to stay
+	Material mat; // this mat here may have to stay
 
 private:
-    // model data
-    std::vector<Mesh> meshes;
+	// model data
+	std::vector<Mesh> meshes;
 
-    void loadModel(std::string path);
-    void processNode(aiNode* node, const aiScene* scene, aiMatrix4x4t<float> transformation, std::string directory, std::vector<Texture>& textures_loaded);
-    Mesh processMesh(aiMesh* mesh, const aiScene* scene, aiMatrix4x4t<float>& transformation, std::string directory, std::vector<Texture>& textures_loaded);
-    std::vector<Texture> loadMaterialTextures(aiMaterial* mat, const aiScene* scene, aiTextureType type,
-        std::string typeName, int slotInc, std::string directory, std::vector<Texture>& textures_loaded);
-    glm::mat4 aiMat4toGLM(aiMatrix4x4t<float>& matrix);
+	void loadModel(std::string path);
+	void processNode(aiNode* node, const aiScene* scene, aiMatrix4x4t<float> transformation, std::string directory, std::vector<Texture>& textures_loaded);
+	Mesh processMesh(aiMesh* mesh, const aiScene* scene, aiMatrix4x4t<float>& transformation, std::string directory, std::vector<Texture>& textures_loaded);
+	std::vector<Texture> loadMaterialTextures(aiMaterial* mat, const aiScene* scene, aiTextureType type,
+		std::string typeName, int slotInc, std::string directory, std::vector<Texture>& textures_loaded);
+	glm::mat4 aiMat4toGLM(aiMatrix4x4t<float>& matrix);
 };
 
 #endif
