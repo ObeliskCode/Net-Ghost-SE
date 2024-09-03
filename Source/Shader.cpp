d5169f3568f0ebe6343f89f517cd96f7420d1818
brent hartshorn <brenthartshorn@proton.me>
2024-09-03 07:27:10 -0700

    DEBUG_SHADERS
    https://github.com/ObeliskCode/Net-Ghost-SE/issues/4

--- Source/Shader.cpp
+++ Source/Shader.cpp
@@ -15,6 +15,10 @@ std::string get_file_contents(const char* filename) {
 }
 
 void Shader::set_vshader(std::string s){
+#ifdef DEBUG_SHADERS
+	std::cout << "------------------------------" << std::endl;
+	std::cout << "set_vshader:" << std::endl << s << std::endl;
+#endif
 	const char* vs = s.c_str();
 	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vs, NULL);
@@ -24,6 +28,10 @@ void Shader::set_vshader(std::string s){
 }
 
 void Shader::set_fshader(std::string s){
+#ifdef DEBUG_SHADERS
+	std::cout << "------------------------------" << std::endl;
+	std::cout << "set_fshader:" << std::endl << s << std::endl;
+#endif
 	const char* fs = s.c_str();
 	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fs, NULL);
@@ -31,7 +39,6 @@ void Shader::set_fshader(std::string s){
 	checkCompileErrors(fragmentShader, "FRAGMENT");
 	this->fid = fragmentShader;
 }
-
 void Shader::compile(){
 	ID = glCreateProgram();
 	glAttachShader(ID, this->vid);
