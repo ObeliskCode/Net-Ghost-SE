8fc1c87ba7650786d6eaf6c2a776c4662ec47c92
brent hartshorn <brenthartshorn@proton.me>
2024-09-02 18:48:21 -0700

    NOASS

--- Source/Model.h
+++ Source/Model.h
@@ -1,9 +1,11 @@
 #ifndef MODEL_H
 #define MODEL_H
 
-#include <assimp/Importer.hpp>
-#include <assimp/scene.h>
-#include <assimp/postprocess.h>
+#ifndef NOASS
+	#include <assimp/Importer.hpp>
+	#include <assimp/scene.h>
+	#include <assimp/postprocess.h>
+#endif
 
 #include "Mesh.h"
 #include "Material.h"
@@ -30,10 +32,13 @@ private:
 	std::vector<Mesh> meshes;
 
 	void loadModel(std::string path);
+#ifndef NOASS
 	void processNode(aiNode* node, const aiScene* scene, aiMatrix4x4t<float> transformation, std::string directory, std::vector<Texture>& textures_loaded);
 	Mesh processMesh(aiMesh* mesh, const aiScene* scene, aiMatrix4x4t<float>& transformation, std::string directory, std::vector<Texture>& textures_loaded);
 	std::vector<Texture> loadMaterialTextures(aiMaterial* mat, const aiScene* scene, aiTextureType type,
 		std::string typeName, int slotInc, std::string directory, std::vector<Texture>& textures_loaded);
+#endif
+
 	glm::mat4 aiMat4toGLM(aiMatrix4x4t<float>& matrix);
 };
 
