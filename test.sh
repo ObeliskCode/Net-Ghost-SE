09f01a53194eb904c430918aff05dd5a5dbd41e2
brent hartshorn <brenthartshorn@proton.me>
2024-09-06 11:18:40 -0700

    testing WASM

--- test.sh
+++ test.sh
@@ -2,3 +2,6 @@
 
 python3 build.py ./Resources/test-shader-wireframe.blend
 python3 build.py ./Resources/test-script-rotate.blend
+
+python3 build.py ./Resources/test-shader-wireframe.blend  --wasm
+python3 build.py ./Resources/test-script-rotate.blend  --wasm
