fcc79467c564b80c27ea5ee39a6ab2d92bef9254
Noel <n6garcia@ucsd.edu>
2024-10-20 19:04:56 -0700

    tabs

--- Tools/c3blender.py
+++ Tools/c3blender.py
@@ -1,2 +1,55 @@
 #!/usr/bin/env python3
 import subprocess, sys, os
+
+_thisdir = os.path.split(os.path.abspath(__file__))[0]
+sys.path.append(_thisdir)
+
+C3 = '/usr/local/bin/c3c'
+
+islinux=iswindows=c3gz=c3zip=None
+if sys.platform == 'win32':
+	BLENDER = 'C:/Program Files/Blender Foundation/Blender 4.2/blender.exe'
+	c3zip = 'https://github.com/c3lang/c3c/releases/download/latest/c3-windows.zip'
+	C3 = os.path.join(_thisdir,'c3/c3c.exe')
+	iswindows=True
+elif sys.platform == 'darwin':
+	BLENDER = '/Applications/Blender.app/Contents/MacOS/Blender'
+	c3zip = 'https://github.com/c3lang/c3c/releases/download/latest/c3-macos.zip'
+else:
+	BLENDER = 'blender'
+	c3gz = 'https://github.com/c3lang/c3c/releases/download/latest/c3-ubuntu-20.tar.gz'
+	islinux=True
+
+if not os.path.isfile(C3):
+	C3 = '/opt/c3/c3c'
+	if not os.path.isfile(C3):
+		if not os.path.isdir('./c3'):
+			if c3gz:
+				if not os.path.isfile('c3-ubuntu-20.tar.gz'):
+					cmd = 'wget -c %s' % c3gz
+					print(cmd)
+					subprocess.check_call(cmd.split())
+				cmd = 'tar -xvf c3-ubuntu-20.tar.gz'
+				print(cmd)
+				subprocess.check_call(cmd.split())
+			elif c3zip and iswindows:
+				if not os.path.isfile('c3-windows.zip'):
+					cmd = ['curl', '-o', 'c3-windows.zip', c3zip]
+					print(cmd)
+					subprocess.check_call(cmd)
+				cmd = ['tar', '-xf', './c3-windows.zip']
+				print(cmd)
+				subprocess.check_call(cmd)
+			elif c3zip:
+				if not os.path.isfile('c3-macos.zip'):
+					cmd = ['curl', '-o', 'c3-macos.zip', c3zip]
+					print(cmd)
+					subprocess.check_call(cmd)
+
+		if islinux:
+			C3 = os.path.abspath('./c3/c3c')
+		elif iswindows:
+			C3 = os.path.abspath('./c3/c3c.exe')
+
+print('c3c:', C3)
+assert os.path.isfile(C3)
